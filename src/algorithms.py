741ebd02dc176860f754a2a80254cb558e5e4d43
PINKTAIGAH <giorgio.b.campanella@gmail.com>
2023-04-29 20:53:55 +0100

    Added full SIRS algorithm

--- src/algorithms.py
+++ src/algorithms.py
@@ -2,7 +2,12 @@ import numpy as np
 
 class Algorithms(object):
 #==============================================================
-#  Class comtainiing algorithms for SIRS model simulation
+# Class comtainiing algorithms for SIRS model simulation
+# Keys for SIRS array:
+# 0 ==> (S)ucceptable
+# 1 ==> (I)nfected
+# 2 ==> (R)ecovered
+# 3 ==> (Im)mune
 
     def __init__(self, N, p1, p2, p3):
         #======================================================
@@ -13,4 +18,77 @@ class Algorithms(object):
         self.p2= p2
         self.p3= p3
 
-    
+    def generateRandomIndex(self):
+        #======================================================
+        # Generate a random index within the bounds of an array
+
+        self.index= (np.random.randint(0, high= self.N, size= 2))
+
+    def findNeighbourStates(self):
+        #======================================================
+        # Find the state of neighbours for randomly selected array element
+
+        self.U= np.roll(self.array, -1, axis=0)[self.index]
+        self.D= np.roll(self.array, +1, axis=0)[self.index]
+        self.R= np.roll(self.array, +1, axis=1)[self.index]
+        self.L= np.roll(self.array, -1, axis=1)[self.index]
+
+    def isInfected(self):
+        #======================================================
+        # Return boolean stating if neighbour of state is infected
+
+        self.infectedNeighbour= self.U == 1 & self.D == 1 & self.R == 1 & self.L == 1
+
+    def isSucceptable(self):
+        #======================================================
+        # Return boolean stating if neighbour of state is succeptable
+
+        self.succeptableNeighbour= self.U == 0 & self.D == 0 & self.R == 0 & self.L == 0
+
+    def isRecovered(self):
+        #======================================================
+        # Return boolean stating if neighbour of state is succeptable
+
+        self.recoveredNeighbour= self.U == 2 & self.D == 2 & self.R == 2 & self.L == 2
+
+    def applySirsRuleS(self):
+        #======================================================
+        # Apply dedicated SIRS rule for a succeptable element
+
+        if self.infectedNeighbour and np.random.rand <= self.p1:
+            self.array[self.index] = 1
+        
+    def applySirsRuleI(self):
+        #======================================================
+        # Apply dedicated SIRS rule for a succeptable element
+
+        if np.random.rand <= self.p2:
+            self.array[self.index] = 2
+
+    def applySirsRuleR(self):
+        #======================================================
+        # Apply dedicated SIRS rule for a succeptable element
+
+        if np.random.rand <= self.p3:
+            self.array[self.index] = 1
+
+    def updateArray(self, array):
+        # =====================================================
+        # Update array according to SIRS model rules.
+
+        self.array= array
+        self.generateRandomIndex()
+
+        if self.array[self.index] == 0:
+            self.isInfected()
+            self.applySirsRuleS()
+        
+        elif self.array[self.index] == 1:
+            self.applySirsRuleI()
+
+        elif self.array[self.index] == 2:
+            self.applySirsRuleR()
+
+        return self.array
+
+
